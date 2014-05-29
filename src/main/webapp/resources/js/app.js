6537c42232cd7e0b4275245e5cb71ba78563e77c
neimad971 <damientalbot26@gmail.com>
2014-05-29 21:51:44 +0200

    setup of angularJS

--- src/main/webapp/resources/js/app.js
+++ src/main/webapp/resources/js/app.js
@@ -1,2 +1,41 @@
-var learningengine = learningengine || {};
-learningengine.app = angular.module("learningengine", ["ngRoute", "upidamodule"]);
+'use strict';
+
+//(function()
+//{*
+	
+	var app = angular.module('learningengine',['ngRoute']);
+	
+	app.config(['$routeProvider', function($routeProvider) 
+	{
+      $routeProvider.
+        when('/app',
+        {
+          templateUrl: 'resources/views/home.html',
+          controller: 'HomeController'
+        }).
+        when('/app/courses',
+        {
+          templateUrl: 'resources/views/allCourses.html',
+          controller: 'CourseController'
+        }).
+        otherwise(
+        {
+          redirectTo: '/app'
+        });
+    }]);
+
+	
+	app.controller('HomeController', ['$scope', function ($scope) 
+	{
+         $scope.message = "home page";
+    }]);
+	
+	app.controller('CourseController', ['$scope', function ($scope) 
+	{
+         $scope.message = "all courses page";
+    }]);
+	
+	
+
+
+//})();
