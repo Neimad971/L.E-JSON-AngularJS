4ba37c3ffb01de7bd3ea11ff242981b6639401f4
neimad971 <damientalbot26@gmail.com>
2014-06-06 20:48:19 +0200

    courses list with ngGrid

--- src/main/webapp/resources/js/app.js
+++ src/main/webapp/resources/js/app.js
@@ -3,7 +3,7 @@
 //(function()
 //{*
 	
-	var app = angular.module('learningengine',['ngRoute']);
+	var app = angular.module('learningengine',['ngRoute', 'ngGrid']);
 	
 	app.config(['$routeProvider', function($routeProvider) 
 	{
@@ -18,6 +18,11 @@
           templateUrl: 'resources/views/allCourses.html',
           controller: 'CourseController'
         }).
+        when('/app/courses/:courseId',
+        {
+          templateUrl: 'resources/views/oneCourse.html',
+          controller: 'CourseDetailController'
+        }).
         otherwise(
         {
           redirectTo: '/app'
@@ -30,17 +35,69 @@
          $scope.message = "home page";
     }]);
 	
-	app.controller('CourseController', ['$scope', '$http', function ($scope, $http) 
+	
+	
+	app.controller('CourseController', ['$scope', '$http', '$location', function ($scope, $http, $location) 
 	{
 		$scope.message = "all courses page";
+		
+		
          
-         $http.get('app/courses').success(function(data)
+         $http.get('app/courses').success(function(allCourses)
 		 {
-             $scope.courses = data;
-             
-         });
+            // $scope.courses = allCourses;
+             //alert(allCourses[0].name);
+        
          
+		 });  
+         $scope.mySelections =[];
+         //prendre mes course plutot que mes données en dur
+		 $scope.myData = [{name: "fezfezf", desc : "ezdezdz", incription : false},
+		                  {name: "fezfezf", desc : "ezdezdz"},
+		                  {name: "fezfezf", desc : "ezdezdz"},
+		                  {name: "fezfezf", desc : "ezdezdz"},{name: "fezfezf", desc : "ezdezdz"},{name: "fezfezf", desc : "ezdezdz"}];
+ 		
+ 		 $scope.gridOptions = {
+ 			data : 'myData',
+ 			selectedItems : $scope.mySelections,
+ 			multiSelection : false
+ 		 };
+ 		 
+ 		 
+ 		$scope.submitSelectedCourses = function (){
+ 			alert("submit ok");
+ 			
+ 			var input = $scope.mySelections
+ 			//creer une mothode pour faire un post
+ 			//
+ 			$http.post('app/courses', input).success()
+ 		} ;
+ 		 
+		
+       
     }]);
+	
+	
+	
+	app.controller('CourseDetailController', ['$scope', '$http', '$location','$routeParams', function ($scope, $http, $location, $routeParams) 
+	{
+		 
+         $scope.getCourseDetail = function(idCourse) 
+         {
+			 $http.get('app/courses/' + idCourse).success(function(oneCourse)
+	    	 {
+				 
+				 
+				 return $scope.test = oneCourse;
+				 
+				// $location.path('app/courses/'+idCourse);
+				 
+	    	 });
+        	 
+         };
+         
+         
+	}]);
 	
 	
 
